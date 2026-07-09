32b25bcc37e4599a343293535ad90e2fe8476562
bopel-maki-macohi <bopel.maki.macohi@gmail.com>
2026-07-08 19:14:19 -0500

    Commit 1

---
+++ source/maki/proti/Main.hx
@@ -0,0 +1,13 @@
+package maki.proti;
+
+import flixel.FlxGame;
+import openfl.display.Sprite;
+
+class Main extends Sprite
+{
+	public function new()
+	{
+		super();
+		addChild(new FlxGame(320, 180, Proti));
+	}
+}
---
+++ source/maki/proti/Proti.hx
@@ -0,0 +1,16 @@
+package maki.proti;
+
+import flixel.FlxState;
+
+class Proti extends FlxState
+{
+	override public function create()
+	{
+		super.create();
+	}
+
+	override public function update(elapsed:Float)
+	{
+		super.update(elapsed);
+	}
+}
